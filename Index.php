3950e5d62d4f513505336d2be02b861958001423
jb <jbaptiste.daniel@gmail.com>
2015-05-19 13:28:01 +0200

    yeahhhh test commit

---
+++ Index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jb
+ * Date: 19/05/15
+ * Time: 13:26
+ */
---
+++ class/contact.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jb
+ * Date: 19/05/15
+ * Time: 10:54
+ */
+
+class contact {
+
+    function create_contact()
+    {
+        echo "yeah";
+        return $this->contact();
+    }
+
+}
